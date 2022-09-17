b66e740ba2388a1c062feb931284fb2055804c33
RahimCml <rhmcml@mail.ru>
2022-09-17 04:11:21 +0400

    Move colors to global styles

---
+++ lib/styles/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color mainColor = Colors.green.shade300;
+  static Color errorColor = Colors.red.shade300;
+  static Color focusColor = Colors.blue.shade300;
+  static Color textColor = Colors.black;
+  static Color versionColorWhite = Colors.white;
+}
