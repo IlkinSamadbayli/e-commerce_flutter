e2dd5126e7f5aebfd59791e8bacd6f42eb348d9d
RahimCml <rhmcml@mail.ru>
2022-09-17 04:32:36 +0400

    Create onboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'home_page.dart';
+import 'pages/onboard_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,11 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(
-        isFocused: true,
-        suffixButtonIcon: Icons.abc_outlined,
-        text: 'button',
-      ),
+      home: const OnboardPage(),
     );
   }
 }
